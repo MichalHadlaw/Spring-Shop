274c9176253781bf1df0df9aa06c618f56191373
Hadław Michał <michalhadlaw@gmail.com>
2023-06-19 12:19:24 +0200

    first commit

---
+++ src/main/java/com/example/securingweb/Product.java
@@ -0,0 +1,46 @@
+package com.example.securingweb;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    private int id;
+    private float price;
+    private String name;
+    private String brand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
+
